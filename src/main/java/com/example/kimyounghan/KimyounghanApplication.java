4f05883ff012c80f6e984bffbb16a06cab6adcb1
justgotothedesk <wlscjf3877@naver.com>
2024-04-03 13:42:20 +0900

    Initial commit

---
+++ src/main/java/com/example/kimyounghan/KimyounghanApplication.java
@@ -0,0 +1,13 @@
+package com.example.kimyounghan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KimyounghanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KimyounghanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kimyounghan/KimyounghanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kimyounghan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KimyounghanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
