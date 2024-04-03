92e88810f8844670b6cb127ffcb4e0f17216f7d5
justgotothedesk <wlscjf3877@naver.com>
2024-04-03 14:53:04 +0900

    feat: HTTP GET 요청에서 걸린 시간을 측정하는 V1 코드 및 테스트 코드 추가

---
+++ src/main/java/com/example/kimyounghan/app/v1/OrderServiceV1.java
@@ -0,0 +1,27 @@
+package com.example.kimyounghan.app.v1;
+
+import com.example.kimyounghan.trace.TraceStatus;
+import com.example.kimyounghan.trace.hellotrace.HelloTraceV1;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV1 {
+
+    private final OrderRepositoryV1 orderRepository;
+    private final HelloTraceV1 trace;
+
+    public void orderItem(String itemId) {
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderService.orderItem()");
+            orderRepository.save(itemId);
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+}
---
+++ src/test/java/com/example/kimyounghan/trace/hellotrace/HelloTraceV1Test.java
@@ -0,0 +1,21 @@
+package com.example.kimyounghan.trace.hellotrace;
+
+import com.example.kimyounghan.trace.TraceStatus;
+import org.junit.jupiter.api.Test;
+
+public class HelloTraceV1Test {
+
+    @Test
+    void begin_end() {
+        HelloTraceV1 trace = new HelloTraceV1();
+        TraceStatus status = trace.begin("hello");
+        trace.end(status);
+    }
+
+    @Test
+    void begin_exception() {
+        HelloTraceV1 trace = new HelloTraceV1();
+        TraceStatus status = trace.begin("hello");
+        trace.exception(status, new IllegalStateException());
+    }
+}
