2074d7015f30c3ef491b26f2e20b89873ffeffc9
justgotothedesk <wlscjf3877@naver.com>
2024-04-04 11:26:08 +0900

    feat: ThreadLocal을 사용한 동시성 문제 해결 코드 및 테스트 코드 추가

---
+++ src/test/java/com/example/kimyounghan/trace/logtrace/ThreadLocalLogTraceTest.java
@@ -0,0 +1,24 @@
+package com.example.kimyounghan.trace.logtrace;
+
+import com.example.kimyounghan.trace.TraceStatus;
+import org.junit.jupiter.api.Test;
+
+public class ThreadLocalLogTraceTest {
+    ThreadLocalLogTrace trace = new ThreadLocalLogTrace();
+
+    @Test
+    void begin_and_level2() {
+        TraceStatus status1 = trace.begin("hello1");
+        TraceStatus status2 = trace.begin("hello2");
+        trace.end(status2);
+        trace.end(status1);
+    }
+
+    @Test
+    void begin_exception_level2() {
+        TraceStatus status1 = trace.begin("hello1");
+        TraceStatus status2 = trace.begin("hello2");
+        trace.exception(status2, new IllegalStateException());
+        trace.exception(status1, new IllegalStateException());
+    }
+}
