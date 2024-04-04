c1d705b72b75b6175efba6849cd26b4ccc812892
justgotothedesk <wlscjf3877@naver.com>
2024-04-04 11:25:43 +0900

    refactor: ThreadLocal로 의존관계 변경

--- src/main/java/com/example/kimyounghan/LogTraceConfig.java
+++ src/main/java/com/example/kimyounghan/LogTraceConfig.java
@@ -2,6 +2,7 @@ package com.example.kimyounghan;
 
 import com.example.kimyounghan.trace.logtrace.FieldLogTrace;
 import com.example.kimyounghan.trace.logtrace.LogTrace;
+import com.example.kimyounghan.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +10,6 @@ import org.springframework.context.annotation.Configuration;
 public class LogTraceConfig {
     @Bean
     public LogTrace logTrace() {
-        return new FieldLogTrace();
+        return new ThreadLocalLogTrace();
     }
 }
