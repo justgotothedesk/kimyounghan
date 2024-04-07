ecd71c718d4436937b4aa41501151582d1f2d40f
justgotothedesk <wlscjf3877@naver.com>
2024-04-07 15:39:00 +0900

    feat: 캐시 프록시 패턴 코드 및 테스트 코드 추가

--- src/test/java/com/example/kimyounghan/proxy/pureproxy/proxy/ProxyPatternTest.java
+++ src/test/java/com/example/kimyounghan/proxy/pureproxy/proxy/ProxyPatternTest.java
@@ -1,5 +1,6 @@
 package com.example.kimyounghan.proxy.pureproxy.proxy;
 
+import com.example.kimyounghan.proxy.pureproxy.proxy.code.CacheProxy;
 import com.example.kimyounghan.proxy.pureproxy.proxy.code.ProxyPatternClient;
 import com.example.kimyounghan.proxy.pureproxy.proxy.code.RealSubject;
 import org.junit.jupiter.api.Test;
@@ -9,6 +10,18 @@ public class ProxyPatternTest {
     void noProxyTest() {
         RealSubject realSubject = new RealSubject();
         ProxyPatternClient client = new ProxyPatternClient(realSubject);
+
+        client.execute();
+        client.execute();
+        client.execute();
+    }
+
+    @Test
+    void cacheProxyTest() {
+        RealSubject realSubject = new RealSubject();
+        CacheProxy cacheProxy = new CacheProxy(realSubject);
+        ProxyPatternClient client = new ProxyPatternClient(cacheProxy);
+
         client.execute();
         client.execute();
         client.execute();
