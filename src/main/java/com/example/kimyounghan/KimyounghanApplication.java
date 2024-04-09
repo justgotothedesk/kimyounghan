99991b24726bdecc953a7b294968887e292618ad
justgotothedesk <wlscjf3877@naver.com>
2024-04-09 13:58:49 +0900

    refactor: 의존관계 변경

--- src/main/java/com/example/kimyounghan/KimyounghanApplication.java
+++ src/main/java/com/example/kimyounghan/KimyounghanApplication.java
@@ -1,12 +1,19 @@
 package com.example.kimyounghan;
 
+import com.example.kimyounghan.app.trace.logtrace.LogTrace;
+import com.example.kimyounghan.app.trace.logtrace.ThreadLocalLogTrace;
 import com.example.kimyounghan.proxy.config.AppV1Config;
 import com.example.kimyounghan.proxy.config.AppV2Config;
+import com.example.kimyounghan.proxy.config.v1_proxy.ConcreteProxyConfig;
+import com.example.kimyounghan.proxy.config.v1_proxy.InterfaceProxyConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
-@Import({AppV1Config.class, AppV2Config.class})
+// @Import({AppV1Config.class, AppV2Config.class})
+// @Import(InterfaceProxyConfig.class)
+@Import(ConcreteProxyConfig.class)
 @SpringBootApplication (scanBasePackages = "com.example.kimyounghan.proxy")
 public class KimyounghanApplication {
 
@@ -14,4 +21,8 @@ public class KimyounghanApplication {
 		SpringApplication.run(KimyounghanApplication.class, args);
 	}
 
+	@Bean
+	public LogTrace logTrace() {
+		return new ThreadLocalLogTrace();
+	}
 }
