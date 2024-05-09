98aa69bb4a738686f6f6f8c1d762ec5dcf7e02f2
justgotothedesk <wlscjf3877@naver.com>
2024-05-09 15:43:58 +0900

    feat: AspectJ 표현식을 사용한 Advisor 생성 코드 추가

--- src/main/java/com/example/kimyounghan/proxy/config/v5_autoproxy/AutoProxyConfig.java
+++ src/main/java/com/example/kimyounghan/proxy/config/v5_autoproxy/AutoProxyConfig.java
@@ -5,6 +5,7 @@ import com.example.kimyounghan.proxy.config.AppV1Config;
 import com.example.kimyounghan.proxy.config.AppV2Config;
 import com.example.kimyounghan.proxy.config.v3_proxyfactory.advice.LogTraceAdvice;
 import org.springframework.aop.Advisor;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
 import org.springframework.aop.support.NameMatchMethodPointcut;
 import org.springframework.context.annotation.Bean;
@@ -14,11 +15,37 @@ import org.springframework.context.annotation.Import;
 @Configuration
 @Import({AppV1Config.class, AppV2Config.class})
 public class AutoProxyConfig {
-    @Bean
+    // 주석을 하지 않으면, advisor1, 2 모두 등록이 되게 때문에 실행이 두 번 일어남.
+    // @Bean
     public Advisor advisor1(LogTrace logTrace) {
         // Pointcut
         NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
         pointcut.setMappedNames("request*", "order*", "save*");
+
+        // Advice
+        LogTraceAdvice advice = new LogTraceAdvice(logTrace);
+
+        return new DefaultPointcutAdvisor(pointcut, advice);
+    }
+
+    // @Bean
+    public Advisor advisor2(LogTrace logTrace) {
+        // Pointcut
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression("execution(* com.example.kimyounghan.proxy.app..*(..))");
+
+        // Advice
+        LogTraceAdvice advice = new LogTraceAdvice(logTrace);
+
+        return new DefaultPointcutAdvisor(pointcut, advice);
+    }
+
+    @Bean
+    public Advisor advisor3(LogTrace logTrace) {
+        // Pointcut
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression("execution(* com.example.kimyounghan.proxy.app..*(..)) " +
+                "&& !execution(* com.example.kimyounghan.proxy.app..noLog(..)");
 
         // Advice
         LogTraceAdvice advice = new LogTraceAdvice(logTrace);
