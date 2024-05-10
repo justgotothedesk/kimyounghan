936097757b15824a069ba4e805dec4a8006edbd4
justgotothedesk <wlscjf3877@naver.com>
2024-05-10 11:13:27 +0900

    test: AOP 프록시 테스트 코드 추가

---
+++ src/test/java/com/example/kimyounghan/aop/AopTest.java
@@ -0,0 +1,42 @@
+package com.example.kimyounghan.aop;
+
+import com.example.kimyounghan.aop.order.OrderRepository;
+import com.example.kimyounghan.aop.order.OrderService;
+import com.example.kimyounghan.aop.order.aop.AspectV1;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Slf4j
+@SpringBootTest
+@Import(AspectV1.class)
+public class AopTest {
+    private final OrderService orderService;
+    private final OrderRepository orderRepository;
+
+    public AopTest(OrderService orderService, OrderRepository orderRepository) {
+        this.orderService = orderService;
+        this.orderRepository = orderRepository;
+    }
+
+    @Test
+    void aopInfo() {
+        log.info("isAopProxy, orderService={}", AopUtils.isAopProxy(orderService));
+        log.info("isAopProxy, orderRepository={}", AopUtils.isAopProxy(orderRepository));
+    }
+
+    @Test
+    void success() {
+        orderService.orderItem("itemA");
+    }
+
+    @Test
+    void exception() {
+        Assertions.assertThatThrownBy(() -> orderService.orderItem("ex"))
+                .isInstanceOf(IllegalStateException.class);
+    }
+}
